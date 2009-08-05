9dc6b0909a74b100982d190fc3adf4b1be63c08d
ippa <iplord@iplord.com>
2009-08-05 21:36:59 +0200

    organizing

---
+++ lib/chingu.rb
@@ -0,0 +1,25 @@
+#
+#
+#
+#
+require 'rubygems'
+require 'gosu'
+
+%w{	window
+		fpscounter
+		named_resource
+		assets
+    actor
+		advanced_actor
+		data_structures
+		rect
+		animation
+		keymap
+		}.each do |lib|
+      root ||= File.dirname(File.expand_path(__FILE__))
+      require File.join(root,"chingu",lib)
+    end
+
+module Chingu
+  VERSION = "0.0.1"
+end
