e8593c8b921353fe2617c92d94aca328a5e00d1f
ippa <iplord@iplord.com>
2009-10-02 04:27:13 +0200

    new trait 'timer' - adds methods during/after/between/then to game object. Do stuff during a limited time, fire a one-shot after X milliseconds, create timed chains of code.

---
+++ examples/example12.rb
@@ -0,0 +1,67 @@
+require 'rubygems'
+require File.join(File.dirname($0), "..", "lib", "chingu")
+include Gosu
+include Chingu
+
+class Game < Chingu::Window
+  def initialize
+    super(640,400)
+    self.input = {:esc => :exit}
+    self.caption = "Example of Chingus :timer trait (arrows,space,return sets timers that affects @x,@y & @color)"
+    
+    Cube.create(:x => 200, :y => 200)
+  end
+end
+
+class Cube < GameObject
+  has_trait :timer
+  
+  def initialize(options)
+    super
+    @white = Color.new(0xFFFFFFFF)
+    @red = Color.new(0xFFFF0000)
+    @blue = Color.new(0xFF0000FF)
+    self.input = {  :left => :left, :right => :right, :up => :up, :down => :down, 
+                    :space => :shake1, :return => :shake2 }
+  end
+  
+  def left
+    during(500) { @x -= 1 }
+  end
+  
+  def right
+    during(500) { @x += 1 }
+  end
+  
+  def up
+    @color = @red
+    during(500) { @y -= 1 }.then { @color = @white }
+  end
+
+  def down
+    @color = @blue
+    during(500) { @y += 1 }.then { @color = @white }
+  end
+
+  def shake1
+    #
+    # Nesting works too!
+    #
+    during(50) {@y -= 4}.then  { during(100) {@y += 4}.then { during(50) {@y -= 4} } }
+  end
+  
+  def shake2
+    #
+    # Does the exact same as "shake1" but using between() instead of during()
+    #
+    between(0,50) {@y -= 4}
+    between(50,150) {@y += 4}
+    between(150,200) {@y -= 4}
+  end
+  
+  def draw
+    $window.fill_rect([@x, @y, 40, 40], @color)
+  end
+end
+
+Game.new.show
---
+++ lib/chingu/traits/timer.rb
@@ -0,0 +1,92 @@
+#--
+#
+# Chingu -- Game framework built on top of the opengl accelerated gamelib Gosu
+# Copyright (C) 2009 ippa / ippa@rubylicio.us
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+#++
+
+
+module Chingu
+  module Traits
+    #
+    # A chingu trait providing timer-methods to its includer, examples:
+    #   during(300) { @color = Color.new(0xFFFFFFFF) }   # forces @color to white ever update for 300 ms
+    #   after(400) { self.destroy! }                     # destroy object after 400 ms
+    #   between(1000,2000) { self.rotate(10) }           # starting after 1 second, call rotate(10) each update during 1 second
+    #
+    # All the above can be combined with a 'then { do_something }'. For example, a classic shmup damage effect:
+    #   during(100) { @color.alpha = 100 }.then { @color.alpha = 255 }
+    #
+    module Timer 
+      def setup_trait(options)
+        @timer_options = {:debug => false}.merge(options)        
+        
+        #
+        # Timers are saved as an array of arrays where each entry contains:
+        # [start_time, end_time (or nil if one-shot), &block]
+        #
+        @_timers = Array.new
+        super
+      end
+
+      def during(time, &block)
+        ms = milliseconds()
+        @_last_timer = [ms, ms + time, block]
+        @_timers << @_last_timer
+        self
+      end
+      
+      def after(time, &block)
+        ms = milliseconds()
+        @_last_timer = [ms + time, nil, block]
+        @_timers << @_last_timer
+        self
+      end
+      
+      def between(start_time, end_time, &block)
+        ms = milliseconds()
+        @_last_timer = [ms + start_time, ms + end_time, block]
+        @_timers << @_last_timer
+        self
+      end
+      
+      def then(&block)
+        # Is last timer a one-shot?
+        if @_last_timer[1] == nil
+          @_timers << [@_last_timer[0], nil, block] # use one-shots start_time for our trailing "then".
+        else        
+          @_timers << [@_last_timer[1], nil, block] # use durable timers end_time for our trailing "then".
+        end
+      end
+      
+      def update_trait
+        ms = milliseconds()
+        @_timers.each do |start_time, end_time, block|
+          if ms > start_time && (end_time == nil || ms < end_time)
+            block.call
+          end
+        end
+
+        # Remove one-shot timers (only a start_time, no end_time) and all timers which have expired
+        @_timers.reject! { |start_time, end_time, block| (ms > start_time && end_time == nil) || (end_time != nil && ms > end_time) }
+        
+        super
+      end
+      
+    end
+  end
+end
