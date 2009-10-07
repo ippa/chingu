62df2a758bdb4ebb4c10780659ba7bb0aec46ddd
ippa <iplord@iplord.com>
2009-10-07 22:25:22 +0200

    README updates, a simple trait-example. Image#retrofy now returns self for chaining.

--- lib/chingu/core_extensions.rb
+++ lib/chingu/core_extensions.rb
@@ -48,6 +48,7 @@ module Gosu
       glBindTexture(GL_TEXTURE_2D, self.gl_tex_info.tex_name)
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+      self
     end
   end
 end
