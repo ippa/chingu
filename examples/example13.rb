41d7defffd08ee7cbe6401ae332da4367771dd81
ippa <iplord@iplord.com>
2009-10-22 03:26:33 +0200

    new example13.rb shows basic usage of new class HighScoreList

---
+++ examples/example13.rb
@@ -0,0 +1,85 @@
+require 'rubygems'
+require File.join(File.dirname($0), "..", "lib", "chingu")
+include Gosu
+include Chingu
+
+#
+# Demonstrating Chingus HighScoreList-class
+# I couldn't keep myself from spicying it up some though  :P
+#
+class Game < Chingu::Window
+  def initialize
+    super(640,400)
+    self.input = {:esc => :exit}
+    self.caption = "Example of Chingus HighScore class"
+    PulsatingText.create(:text => "HIGH SCORES", :x => $window.width/2, :y => 50, :size => 70)
+    
+    #
+    # Load a list from disk, defaults to "high_score_list.yml"
+    # Argument :size forces list to this size
+    #
+    @high_score_list = HighScoreList.load(:size => 10)
+
+    #
+    # Add some new high scores to the list. :name and :score are required but you can put whatever.
+    # They will mix with the old scores, automatic default sorting on :score
+    #
+    10.times { @high_score_list.add(:name => "NEW", :score => rand(10000)) }
+    
+    #
+    # Iterate through all high scores and create the visual represenation of it
+    #
+    @high_score_list.each_with_index do |high_score, index|
+      y = index * 25 + 100
+      Text.create(:text => high_score[:name], :x => 200, :y => y, :size => 20)
+      Text.create(:text => high_score[:score], :x => 400, :y => y, :size => 20)
+    end
+    
+    # @high_score_list.save  # Uncomment to save list to disk
+  end
+  
+  def update
+    super
+    self.caption = "FPS #{$window.fps} - game objects: #{game_objects.size}"
+  end
+end
+
+#
+# colorful pulsating text...
+#
+class PulsatingText < GameObject
+  has_trait :timer, :effect
+  @@red = Color.new(0xFFFF0000)
+  @@green = Color.new(0xFF00FF00)
+  @@blue = Color.new(0xFF0000FF)
+  
+  def initialize(options)
+    super
+    @text = options[:text]
+    @size = options[:size] || 20
+    @pulse = options[:pulse] || false
+    @image = options[:image] || Image.from_text($window, @text, default_font_name(), @size, 1, 440, :center)
+    
+    self.rotation_center(:center_center)
+        
+    every(20) { create_pulse }   if @pulse == false
+  end
+  
+  def create_pulse
+    pulse = PulsatingText.create(:text => @text, :x => @x, :y => @y, :size => @size, :pulse => true, :image => @image, :zorder => @zorder+1)
+    colors = [@@red, @@green, @@blue]
+    pulse.color = colors[rand(colors.size)].dup
+    pulse.mode = :additive
+    pulse.alpha -= 100
+    pulse.scale_rate = 0.005
+    pulse.fade_rate = -2 -rand(5)
+    pulse.rotation_rate = rand(2)==0 ? 0.05 : -0.05
+  end
+    
+  def update
+    destroy if self.alpha == 0
+  end
+  
+end
+
+Game.new.show
