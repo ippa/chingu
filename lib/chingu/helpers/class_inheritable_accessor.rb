d44dc423d6182dcb691872fd5976b30e2fdd074a
ippa <iplord@iplord.com>
2009-11-26 14:17:28 +0100

    correct namespace for class_inheritable_accessor

--- lib/chingu/helpers/class_inheritable_accessor.rb
+++ lib/chingu/helpers/class_inheritable_accessor.rb
@@ -3,27 +3,29 @@
 # But we use the rails-name for it, class_inheritable_accessor, which should make ppl more @ home.
 #
 module Chingu
-module ClassInheritableAccessor
-  def self.included(base)
-    base.extend(ClassMethods)    
-  end
-
-  module ClassMethods
-    def class_inheritable_accessor(*args)
-      @cattr_inheritable_attrs ||= [:cattr_inheritable_attrs]
-      @cattr_inheritable_attrs += args
-      args.each do |arg|
-        class_eval %(
-          class << self; attr_accessor :#{arg} end
-        )
-      end
-      @cattr_inheritable_attrs
+module Helpers
+  module ClassInheritableAccessor
+    def self.included(base)
+      base.extend(ClassMethods)    
     end
-
-    def inherited(subclass)
-      @cattr_inheritable_attrs.each do |inheritable_attribute|
-        instance_var = "@#{inheritable_attribute}" 
-        subclass.instance_variable_set(instance_var, instance_variable_get(instance_var))
+  
+    module ClassMethods
+      def class_inheritable_accessor(*args)
+        @cattr_inheritable_attrs ||= [:cattr_inheritable_attrs]
+        @cattr_inheritable_attrs += args
+        args.each do |arg|
+          class_eval %(
+            class << self; attr_accessor :#{arg} end
+          )
+        end
+        @cattr_inheritable_attrs
+      end
+  
+      def inherited(subclass)
+        @cattr_inheritable_attrs.each do |inheritable_attribute|
+          instance_var = "@#{inheritable_attribute}" 
+          subclass.instance_variable_set(instance_var, instance_variable_get(instance_var))
+        end
       end
     end
   end
