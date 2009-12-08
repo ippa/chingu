15fd7918d54e4dce8d9bedb9b06308dc845a3836
ippa <iplord@iplord.com>
2009-12-08 05:27:53 +0100

    bump gem

--- lib/chingu.rb
+++ lib/chingu.rb
@@ -33,7 +33,7 @@ require_all "#{CHINGU_ROOT}/chingu/traits"
 require_all "#{CHINGU_ROOT}/chingu"
 
 module Chingu
-  VERSION = "0.6.4"
+  VERSION = "0.6.5"
   
   DEBUG_COLOR = Gosu::Color.new(0xFFFF0000)
   DEBUG_ZORDER = 9999
