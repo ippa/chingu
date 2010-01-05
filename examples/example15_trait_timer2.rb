8fef3868228dcd35d4829799df26710bb139398e
ippa <iplord@iplord.com>
2010-01-05 14:41:37 +0100

    put instructions in example15 caption

--- examples/example15_trait_timer2.rb
+++ examples/example15_trait_timer2.rb
@@ -20,14 +20,15 @@ class Stuff < GameState
   
   def initialize(options = {})
     super
-    self.input = {:t => :toggle, :"1" => :fast, :"2" => :slow}
+    self.input = {:s => :stop, :"1" => :fast, :"2" => :slow}
     @thing = Thing.create(:x => $window.width/2, :y => $window.height / 2 )
     
     every(500, :name => :blink) { @thing.visible? ? @thing.hide! : @thing.show! }
     p timer_exists?(:blink)
+    $window.caption = "Timer-usage. 's' to stop timer, '1' for fast timer, '2' for slow timer"
   end
   
-  def toggle
+  def stop
     stop_timer(:name => :blink)
   end
   
