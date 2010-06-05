7a8465a135361670f43591ad3e98bf31ebc7184e
ippa <ippa@.(none)>
2010-06-05 23:17:24 +0200

    add GameObject#draw_at(x,y)

--- lib/chingu/game_object.rb
+++ lib/chingu/game_object.rb
@@ -149,11 +149,17 @@ module Chingu
     end
     
     #
-    # Works as #draw() but takes offsets for all draw_rot()-arguments. Used among others by by viewport-trait
+    # Works as #draw() but takes offsets for all draw_rot()-arguments. Used among others by the viewport-trait.
     #
     def draw_relative(x=0, y=0, zorder=0, angle=0, center_x=0, center_y=0, factor_x=0, factor_y=0)
       @image.draw_rot(@x+x, @y+y, @zorder+zorder, @angle+angle, @center_x+center_x, @center_y+center_y, @factor_x+factor_x, @factor_y+factor_y, @color, @mode) if @visible
     end
-    
+
+    #
+    # Works as #draw() but takes x/y arguments. Used among others by the edit-game state.
+    #
+    def draw_at(x=0, y=0)
+      @image.draw_rot(x, y, @zorder, @angle, @center_x, @center_y, @factor_x, @factor_y, @color, @mode) if @visible
+    end
   end  
 end
