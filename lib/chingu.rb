e2b01125888d1909b04d631bcedac024d0067f3d
ippa <ippa@.(none)>
2010-06-17 20:20:04 +0200

    small gembump

--- lib/chingu.rb
+++ lib/chingu.rb
@@ -33,7 +33,7 @@ require_all "#{CHINGU_ROOT}/chingu/traits"
 require_all "#{CHINGU_ROOT}/chingu"
 
 module Chingu
-  VERSION = "0.7.6"
+  VERSION = "0.7.6.1"
   
   DEBUG_COLOR = Gosu::Color.new(0xFFFF0000)
   DEBUG_ZORDER = 9999
