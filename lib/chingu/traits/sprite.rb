5be8dfb7fb6a510c560ddd6e24d174ae8d48a6c9
ippa <ippa@.(none)>
2010-06-28 08:32:10 +0200

    complete sprite-trait with missing stuff from GameObject

--- lib/chingu/traits/sprite.rb
+++ lib/chingu/traits/sprite.rb
@@ -89,6 +89,133 @@ module Chingu
       end
 
       #
+      # Get all settings from a game object in one array.
+      # Complemented by the GameObject#attributes= setter.
+      # Makes it easy to clone a objects x,y,angle etc.
+      #
+      def attributes
+        [@x, @y, @angle, @center_x, @center_y, @factor_x, @factor_y, @color, @mode, @zorder]
+      end
+  
+      #
+      # Set all attributes on 1 line
+      # Mainly used in combination with game_object1.attributes = game_object2.attributes
+      #
+      def attributes=(attributes)
+        self.x, self.y, self.angle, self.center_x, self.center_y, self.factor_x, self.factor_y, self.color, self.mode, self.zorder = *attributes
+      end
+
+      #
+      # Set an effective width for the object on screen.
+      # Chingu does this by setting factor_x depending on imge.width and width given.
+      # Usually better to have a large image and make it smaller then the other way around.
+      #
+      def width=(width)
+        @factor_x = width.to_f / @image.width.to_f
+      end
+    
+      # Get effective on width by calculating it from image-width and factor
+      def width
+        (@image.width * @factor_x).abs
+      end
+
+      #
+      # Set an effective height for the object on screen.
+      # Chingu does this by setting factor_x depending on imge.width and width given.
+      # Usually better to have a large image and make it smaller then the other way around.
+      #
+      def height=(height)
+        @factor_y = height.to_f / @image.height.to_f
+      end
+    
+      # Get effective on heightby calculating it from image-width and factor
+      def height
+        (@image.height.to_f * @factor_y).abs
+      end
+
+      # Set width and height in one swoop
+      def size=(size)
+        self.width, self.height = *size
+      end
+    
+      # Get objects width and height in an array
+      def size
+        [self.width, self.height]
+      end
+      
+
+      # Quick way of setting both factor_x and factor_y
+      def factor=(factor)
+        @factor = factor
+        @factor_x = @factor_y = factor
+      end
+      alias scale= factor=
+      alias scale factor
+          
+      # Quick way of setting both center_x and center_y
+      def center=(center)
+        @center = center
+        @center_x = @center_y = center
+      end
+    
+      # Get objects alpha-value (internally stored in @color.alpha)
+      def alpha
+        @color.alpha
+      end
+    
+      # Set objects alpha-value (internally stored in @color.alpha)
+      # If out of range, set to closest working value. this makes fading simpler.
+      def alpha=(value)
+        value = 0   if value < 0
+        value = 255 if value > 255
+        @color.alpha = value
+      end
+
+      #
+      # Sets angle, normalize it to between 0..360
+      #
+      def angle=(value)
+        if value < 0
+          value = 360+value
+        elsif value > 360
+          value = value-360
+        end
+        @angle = value
+      end
+
+      #
+      # Disable automatic calling of draw and draw_trait each game loop
+      #
+      def hide!
+        @visible = false
+      end
+    
+      #
+      # Enable automatic calling of draw and draw_trait each game loop
+      #
+      def show!
+        @visible = true
+      end
+    
+      #
+      # Returns true if visible (not hidden)
+      #
+      def visible?
+        @visible == true
+      end    
+
+
+      # Returns true if object is inside the game window, false if outside
+      def inside_window?(x = @x, y = @y)
+        x >= 0 && x <= $window.width && y >= 0 && y <= $window.height
+      end
+
+      # Returns true object is outside the game window 
+      def outside_window?(x = @x, y = @y)
+        not inside_window?(x,y)
+      end
+
+      #
       # Our encapsulation of GOSU's image.draw_rot, uses the objects variables to draw it on screen if @visible is true
       #
       def draw
