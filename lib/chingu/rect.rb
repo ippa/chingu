f899a89bd73c466a5094be6f712a2651c2ddf569
Bil Bas <bil.bagpuss@gmail.com>
2010-08-17 13:15:20 +0100

    Removed redundant RubyGame specific stuff from Rect (specifically class Surface), since it is coming up in rdoc.

--- lib/chingu/rect.rb
+++ lib/chingu/rect.rb
@@ -619,13 +619,4 @@ class Rect < Array
 
 end # class Rect
 
-
-class Surface
-	# Return a Rect with the same width and height as the Surface, positioned
-	# at (0,0).
-	def make_rect()
-		return Rect.new(0,0,self.width,self.height)
-	end
-end
-
-end # module Rubygame
+end # module Chingu
