3f174cf8d44f71f5b10850b86936e555004117ef
ippa <ippa@.(none)>
2010-08-17 23:14:27 +0200

    dup @color in GameObject.attributes for less confusion when doing new_game_object.attributes = game_object.attributes

--- lib/chingu/game_object.rb
+++ lib/chingu/game_object.rb
@@ -100,7 +100,7 @@ module Chingu
     # Makes it easy to clone a objects x,y,angle etc.
     #
     def attributes
-      [@x, @y, @angle, @center_x, @center_y, @factor_x, @factor_y, @color, @mode, @zorder]
+      [@x, @y, @angle, @center_x, @center_y, @factor_x, @factor_y, @color.dup, @mode, @zorder]
     end
 
     #
