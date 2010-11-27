a5291215b5aa7db10e8646b9ce6cd5878ad661bb
ippa <ippa@.(none)>
2010-11-27 10:39:22 +0100

    small cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,16 +7,13 @@ require 'rspec'
 require 'chingu/require_all'
 
 require 'chingu'
-require 'chingu/helpers/options_setter'
 
 def media_path(file)
   File.join($window.root, "..", "..", "examples", "media", file)
 end
 
 if defined?(Rcov)
-
   # all_app_files = Dir.glob('lib/**/*.rb')
   # all_app_files.each{|rb| require rb}
-
 end
 
