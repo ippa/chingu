8f3cf91bf5ea03aba40d2f9f8c17a09002c4a16f
ippa <ippa@.(none)>
2010-12-12 00:01:12 +0100

    bump gem to 0.8rc3

--- lib/chingu.rb
+++ lib/chingu.rb
@@ -37,8 +37,7 @@ require_all "#{CHINGU_ROOT}/chingu/traits"
 require_all "#{CHINGU_ROOT}/chingu"
 
 module Chingu
-  # VERSION = "0.7.7.5"
-  VERSION = "0.8rc2"
+  VERSION = "0.8rc3"
   
   DEBUG_COLOR = Gosu::Color.new(0xFFFF0000)
   DEBUG_ZORDER = 9999
