4a0c233abe82ad2db043796dc2cda29a2fd86790
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-10-06 16:55:19 -0700

    show more helpful error message for test fail; whitespace formatting

--- spec/chingu/input_spec.rb
+++ spec/chingu/input_spec.rb
@@ -1,21 +1,28 @@
 require 'spec_helper'
 
 describe Chingu::Input do
+
   it "should map all defined Gosu input constants to Chinu symbols" do
+
     # Simpler if all the inputs are in a big hash.
     input_names = Gosu.constants.select {|constant| constant =~ /^(?:Kb|Ms|Gp)/ }
     input_names.delete_if {|name| name.to_s =~ /Range(?:Start|End)|Num$/ } # Special entries.
+
     gosu_inputs = input_names.inject({}) {|hash, name| hash[name] = Gosu.const_get name; hash }
 
-    gosu_inputs.each_value do |code|
-      next if code==0 # todo, check into this, spooner? ;)
+    gosu_inputs.each do | key, code|
+      next if code == 0 # todo, check into this, spooner? ;)
+
       symbols = described_class::CONSTANT_TO_SYMBOL[code]
-      symbols.should_not be_empty
+      symbols.should_not be_empty, "Error: symbols #{symbols} should not be empty.  code #{code} for key #{key} gave us nil: #{symbols}"
+
       symbols.each {|s| s.should be_kind_of Symbol }
     end
+
   end
 
   it "should map all Chingu symbols to Gosu input codes" do
+
     described_class::CONSTANT_TO_SYMBOL.values.flatten.uniq.each do |symbol|
       described_class::SYMBOL_TO_CONSTANT[symbol].should be_kind_of Integer
     end
