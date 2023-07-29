63d4b33b1083e0855ea24e3951e7822291aaacf8
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-07-29 20:19:04 -0300

    Requiring rubygems is implicitly done so since Ruby 1.9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'rubygems'
 require 'rspec'
 require 'chingu/require_all'
 
@@ -16,4 +16,3 @@ if defined?(Rcov)
   # all_app_files = Dir.glob('lib/**/*.rb')
   # all_app_files.each{|rb| require rb}
 end
-
