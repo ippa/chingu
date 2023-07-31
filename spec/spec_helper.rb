ef0b018d1615e3bcc8adf079530bbbb7adac3d2c
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-07-31 12:33:39 -0300

    Remove use of Rcov, and magical comment with encoding
    Since Ruby 2.0, utf-8 is used as default encoding

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,13 @@
-# encoding: utf-8
 
+# Add lib/ to load path
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rspec'
-require 'chingu/require_all'
 
 require 'chingu'
+require 'chingu/require_all'
 
 def media_path(file)
-  File.join($window.root, "..", "..", "examples", "media", file)
-end
-
-if defined?(Rcov)
-  # all_app_files = Dir.glob('lib/**/*.rb')
-  # all_app_files.each{|rb| require rb}
+  File.join($window.root, '..', '..', 'examples', 'media', file)
 end
